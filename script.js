db857725515e8bd94130b2dcfb0e24cfc7fa9839
Magooch <crvallecito@gmail.com>
2024-06-19 15:25:36 -0600

    Add btn variable and connect to the button in html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const k = document.querySelector('.k');
+const k = document.getElementById('k');
 const c = k - 273.15;
 const f = ((k - 273.15) * 1.8) + 32;
-
+const btn = document.getElementById('btn');
