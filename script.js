aa23543f29e7d7919868a7c0d64817ecf4a2bda2
Magooch <crvallecito@gmail.com>
2024-06-19 17:05:03 -0600

    Create function that converts Kelvin to C and F

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const k = document.getElementById('k');
-const c = k - 273.15;
-const f = ((k - 273.15) * 1.8) + 32;
-const btn = document.getElementById('btn');
+const k = document.getElementById("k");
+const btn = document.getElementById("btn");
+
+const ct = document.getElementById("convert_text");
+
+function convert() {
+    if (k.value <= 0) {
+        let c = k.value - 273.15
+        let f = ((k.value - 273.15) * 1.8) + 32
+        ct.textContent = `Celsius: ${c} --- Fahrenheit: ${f}`
+    } else {
+        let c = k.value - 273.15
+        let f = ((k.value - 273.15) * 1.8) + 32
+        ct.textContent = `Celsius: ${c} --- Fahrenheit: ${f}`
+    }
+}
+btn.addEventListener('click', convert);
