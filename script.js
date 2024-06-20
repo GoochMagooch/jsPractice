07602b41e41b810478d336a08f0e001ff3f82484
Magooch <crvallecito@gmail.com>
2024-06-20 13:34:28 -0600

    Remove previous code blocks

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*
-const k = document.getElementById("k");
-const btn = document.getElementById("btn");
-const ct = document.getElementById("convert_text");
-
-function convert() {
-    if (k.value <= 0) {
-        let c = k.value - 273.15
-        let f = ((k.value - 273.15) * 1.8) + 32
-        ct.textContent = `Celsius: ${c} --- Fahrenheit: ${f}`
-    } else {
-        let c = k.value - 273.15
-        let f = ((k.value - 273.15) * 1.8) + 32
-        ct.textContent = `Celsius: ${c} --- Fahrenheit: ${f}`
-    }
-}
-btn.addEventListener('click', convert);
-*/
-
 const k = document.getElementById("k");
 const btn = document.getElementById("btn");
 const ct = document.getElementById("convert_text");
